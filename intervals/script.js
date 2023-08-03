e42cf4f75512d235a870d602b791d222f323bdf0
Javier <Jvasquez5422@pelhamprepacademy.org>
2023-08-03 16:44:18 -0400

    Finsihed everything

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,5 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+
+setInterval(function () {counterElement.innerHTML = time += 1}, 1000);
